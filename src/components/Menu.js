ae4cee5bebe7738d7bf23d4c78f1a981315f83b6
Héctor Fuentes <hectorfuentesg@gmail.com>
2019-08-20 00:03:10 -0400

    Pruebas en menú

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -7,7 +7,7 @@ export default class Menu extends React.PureComponent {
     render() {
         return (
             <nav className="navbar fixed-bottom navbar-expand-sm navbar-dark bg-dark">
-                <a className="navbar-brand" href="#">Bottom navbar</a>
+                <a className="navbar-brand" href="#">Eventos</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse"
                     aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
@@ -15,21 +15,22 @@ export default class Menu extends React.PureComponent {
                 <div className="collapse navbar-collapse" id="navbarCollapse">
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item active">
-                            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                            <a className="nav-link" href="#">Inicio <span className="sr-only">(current)</span></a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Link</a>
+                            <a className="nav-link" href="#">Admin</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link disabled" href="#">Disabled</a>
+                            <a className="nav-link disabled" href="#">CPR</a>
                         </li>
                         <li className="nav-item dropup">
                             <a className="nav-link dropdown-toggle" href="#" id="dropdown10" data-toggle="dropdown" aria-haspopup="true"
-                                aria-expanded="false">Dropup</a>
+                                aria-expanded="false">Ver</a>
                             <div className="dropdown-menu" aria-labelledby="dropdown10">
-                                <a className="dropdown-item" href="#">Action</a>
-                                <a className="dropdown-item" href="#">Another action</a>
-                                <a className="dropdown-item" href="#">Something else here</a>
+                                <a className="dropdown-item" href="#">Todos</a>
+                                <a className="dropdown-item" href="#">Créditos</a>
+                                <a className="dropdown-item" href="#">Cumpleaños</a>
+                                <a className="dropdown-item" href="#">Eventos</a>
                             </div>
                         </li>
                     </ul>
