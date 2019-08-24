161e34b48c629b8be3791ff852cb15ab842b0922
Héctor Fuentes <hectorfuentesg@gmail.com>
2019-08-24 00:41:17 -0400

    se agrega codigo para implemetar funciones firebase

---
+++ src/services/Firebase.js
@@ -0,0 +1,15 @@
+class Firebase {
+    getConfig() {
+        return {
+            apiKey: "AIzaSyAkQF5KPZE6eC_gv4uionqEMnHxFVOz_Z8",
+            authDomain: "eventos-react.firebaseapp.com",
+            databaseURL: "https://eventos-react.firebaseio.com",
+            projectId: "eventos-react",
+            storageBucket: "",
+            messagingSenderId: "100043971091",
+            appId: "1:100043971091:web:4e8a3209a4580b36"
+        };
+    }
+}
+
+export default new Firebase();
